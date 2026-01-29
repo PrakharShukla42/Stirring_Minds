14f2b5b708e0a6b21d132f4c9b4e996905534c08
Prakhar Shukla <therockstarop9956@gmail.com>
2026-01-30 01:43:35 +0530

    Backend With routes/authRoutes.js Created

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 
@@ -13,6 +14,8 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
+
 
 app.get("/", (req, res) => {
   res.send("API is running...");
